323b63d9ae754805ddcb8459a2995e1d11274c3d
Evgeny V. Generalov <e.generalov@gmail.com>
2012-04-28 20:41:04 +0600

    extract settings in runtests

--- runtests.py
+++ runtests.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
-import os
-import sys
-from django.conf import settings
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    },
+    'slave': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+INSTALLED_APPS = ['django_replicated']
+
 
 if __name__ == "__main__":
-    settings.configure(
-        DATABASES={
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': ':memory:',
-            },
-            'slave': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': ':memory:',
-            }
-        },
-        INSTALLED_APPS = ['django_replicated']
-    )
+    import sys
+    from django.conf import settings
     from django.core.management import execute_from_command_line
 
-    print sys.argv
+    settings.configure(**dict([(k,v) for k,v in globals().items() if k.isupper()]))
     execute_from_command_line(sys.argv[:1] + ['test'] + sys.argv[1:])
